14614b083c5c00ab171b2583de06683d1280085d
Devanshi Malaviya <devanshimalaviya1@gmail.com>
2020-07-27 13:56:28 -0400

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<?php  
+require 'config/config.php';
+if (isset($_SESSION['username'])) {
+	$userLoggedIn = $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+	$user = mysqli_fetch_array($user_details_query);
+}
+else {
+	header("Location: register.php");
+}
+?>
+
+<html> 
+<head>
+    <title>Welcome to conneXt</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div class="top_bar">
+        <div class="logo">
+            <a href="index.php">conneXt</a>
+        </div>
+        <nav>
+            <a href="<?php echo $userLoggedIn; ?>">
+				<?php echo $user['first_name']; ?>
+			</a>
+            <a href = "index.php"> Home </a>
+            <a href = "#"> Messages </a>
+            <a href = "#"> Notifications </a>
+            <a href = "#"> Users </a>
+            <a href = "#"> Settings </a>
+            <a href = "includes/handlers/logout.php"> Log out</a>
+
+
+        </nav>
+    </div>
+    <div class="wrapper">
